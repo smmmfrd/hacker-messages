073c0448ab359cff0f74bef833d9c261a02bdd14
smmmfrd <smmmfrd@gmail.com>
2023-02-08 14:09:27 -0800

    Option Menu Styling

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,8 +5,16 @@ export default function Layout({ children }) {
 
     return (
         <>
-            <button onClick={() => setShowOptions(!showOptions)} className="absolute right-0 px-3 text-3xl">&equiv;</button>
-            {children}
+            <button onClick={() => setShowOptions(!showOptions)} className={`absolute ${ showOptions ? "right-28" : "right-0"} px-3 text-3xl duration-75`}>&equiv;</button>
+            <div className="flex flex-row-reverse overflow-hidden">
+                <nav className={`${showOptions ? "w-28" : "w-0"} truncate flex-none duration-75`}>
+                    <ul>
+                        <li>Change User</li>
+                        <li>Enter Sandbox</li>
+                    </ul>
+                </nav>
+                {children}
+            </div>
         </>
     )
 }
