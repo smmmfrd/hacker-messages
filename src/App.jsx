08e10309c8795ad7f52741fdad84bf34facbb8ee
smmmfrd <smmmfrd@gmail.com>
2023-02-07 17:38:46 -0800

    Initial Commit and Tailwind Setup

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="">
+            <h1 className="text-3xl underline font-bold">Hello Vite + Tailwind</h1>
+        </div>
+    )
+}
