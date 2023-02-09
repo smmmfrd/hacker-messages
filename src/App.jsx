207e89f89dea3ffc59d94489cca036dad47a5923
smmmfrd <smmmfrd@gmail.com>
2023-02-09 10:43:28 -0800

    Header Move and Positioning Fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,29 @@ import { useState } from "react"
 export default function App() {
     const [messageOpen, setMessageOpen] = useState(false);
 
-    return (
-        <main className="flex mx-auto justify-center py-8">
-            <div className="w-6">
-                <button title="New Message" className="w-full bg-highlight text-background">+</button>
-            </div>
-            <div className="w-80 border border-yellow-400 p-4 flex flex-col gap-2">
-                <h1 className="text-3xl font-bold">Welcome, User</h1>
-                <section className="border-4 border-highlight px-2 py-1 hover:bg-highlight hover:text-background hover:border-highlight" onClick={() => setMessageOpen(!messageOpen)}>
+    function testMessages() {
+        let messages = [];
+        for (let i = 0; i < 20; i++) {
+            messages.push(
+                <section key={i} className="border-4 border-highlight px-2 py-1 hover:bg-highlight hover:text-background hover:border-highlight" onClick={() => setMessageOpen(!messageOpen)}>
                     <h2>URGENT</h2>
                     <p>This is a message that is so long...</p>
                 </section>
+            );
+        }
+        return messages;
+    }
+
+    return (
+        <main className="flex mx-auto justify-center">
+            <div className="w-80 border-b border-x border-highlight p-4 flex flex-col gap-2">
+                {testMessages()}
             </div>
-            <div className={`${messageOpen ? "w-full border border-l-0" : "w-0 border-0"} duration-100 truncate border-highlight`}>
+            <div className={`${messageOpen ? "w-full border-b border-r" : "w-0 border-0"} h-max duration-100 truncate border-highlight`}>
                 <button onClick={() => setMessageOpen(false)} title="Close Message" className="w-10 h-10 bg-highlight text-background">{'<'}</button>
                 <h2>Message Title</h2>
                 <p>Message Text</p>
             </div>
         </main>
-    )
+    );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,10 +5,15 @@ export default function Layout({ children }) {
 
     return (
         <>
-            <button onClick={() => setShowOptions(!showOptions)} className={`absolute ${ showOptions ? "right-28" : "right-0"} px-3 text-3xl duration-75`}>&equiv;</button>
-            <div className="flex flex-row-reverse overflow-hidden">
-                <nav className={`${showOptions ? "w-28" : "w-0"} truncate flex-none duration-75`}>
-                    <ul>
+            <header className="sticky top-0 bg-background border-b border-b-highlight">
+                <div className="mx-auto">
+                    <h1 className="text-3xl sm:text-center font-bold">Welcome, User</h1>
+                </div>
+                <button onClick={() => setShowOptions(!showOptions)} className={`absolute top-0 right-0 px-3 text-3xl duration-75 ${showOptions ? "mr-28" : "mr-0"}`}>&equiv;</button>
+            </header>
+            <div className="flex flex-row-reverse overflow-x-hidden">
+                <nav className={`right-0 ${showOptions ? "w-28" : "w-0"} truncate flex-none duration-75`}>
+                    <ul className="fixed">
                         <li>Change User</li>
                         <li>Enter Sandbox</li>
                     </ul>
